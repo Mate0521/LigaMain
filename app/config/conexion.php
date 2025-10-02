c16bd5efa497877237a17da364379727ef22f0ef
Mateo Card <mateo.cardenas.pedraza@gmail.com>
2025-10-02 02:48:00 -0500

    adicion de arquitectura base para la aplicacion

---
+++ app/config/conexion.php
@@ -0,0 +1,35 @@
+<?php
+
+class Conexion{
+    private $conexion;
+    private $resultado;
+    
+    public function abrir(){
+        //$this -> conexion = new mysqli("localhost", "root", "", "liga_main");
+        $this -> conexion = new mysqli(
+            "10.123.0.165", 
+            "itiud_aplint", 
+            "GYesgQ118&", 
+            "itiud_aplint");
+    }
+    
+    public function cerrar(){
+        $this -> conexion -> close();
+    }
+    
+    public function ejecutar($sentencia){
+        $this -> resultado = $this -> conexion -> query($sentencia);
+    }
+    
+    public function registro(){
+        return $this -> resultado -> fetch_row();
+    }
+    
+    public function filas(){
+        return $this -> resultado -> num_rows;
+    }
+    
+}
+
+
+?>
