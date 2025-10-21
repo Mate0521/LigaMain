a9e1763ad8b725e8eb825e46b41f4201b8f0924c
Mateo Card <mateo.cardenas.pedraza@gmail.com>
2025-10-20 19:29:30 -0500

    mas correciones

--- views/edicionPartido.php
+++ views/edicionPartido.php
@@ -22,7 +22,7 @@ if (isset($_POST["actualizar"])) {
 
 <div class="container text-center mt-4">
     <h3>Jugar Partido</h3>
-    <form method="POST" class="d-inline-block bg-light text-dark p-4 rounded shadow" action="index.php?pid=PanelCam&idPartido=<?= $_GET['idPartido'] ?>&id_cam=<?= $_GET['id_cam'] ?>" name="actualizar">
+    <form method="POST" class="d-inline-block bg-light text-dark p-4 rounded shadow" action="index.php?pid=PanelCam" name="actualizar">
         <input type="hidden" name="id_cam" value="<?php echo $_GET['id_cam']; ?>">
 
         <div class="mb-3">
